c845f6a6ee68f6b58a781fec7419f58101db6836
Matthias Bolte <matthias@tinkerforge.com>
2015-08-12 11:15:13 +0200

    Fix minor warnings in C/C++ examples

--- software/examples/c/example_simple.c
+++ software/examples/c/example_simple.c
@@ -7,7 +7,7 @@
 #define PORT 4223
 #define UID "XYZ" // Change to your UID
 
-int main() {
+int main(void) {
 	// Create IP connection
 	IPConnection ipcon;
 	ipcon_create(&ipcon);
@@ -19,7 +19,7 @@ int main() {
 	// Connect to brickd
 	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
 		fprintf(stderr, "Could not connect\n");
-		exit(1);
+		return 1;
 	}
 	// Don't use device before ipcon is connected
 
@@ -27,7 +27,7 @@ int main() {
 	uint16_t distance_value;
 	if(distance_us_get_distance_value(&dus, &distance_value) < 0) {
 		fprintf(stderr, "Could not get distance value, probably timeout\n");
-		exit(1);
+		return 1;
 	}
 
 	printf("Distance Value: %d\n", distance_value);
@@ -35,4 +35,5 @@ int main() {
 	printf("Press key to exit\n");
 	getchar();
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
+	return 0;
 }
